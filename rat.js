b8108acbdf61fedfeda5566797c16dbe3daad258
Flint <oliverflintrose@gmail.com>
2023-03-31 23:31:59 -0500

    Trim() finished, takes inputed string and returns array full of each unique word contained.
    git push

--- rat.js
+++ rat.js
@@ -5,26 +5,57 @@ const learningRate = 0.3;
 //RNN , Recurrent Neural Network 
 
 
-
 function error(err, inputD) {
     if (err) throw err;
     return inputD;
 }
 
-//var paul = fs.readFileSync('text/message.txt', error);
+var paul = fs.readFileSync('text/message.txt', error);
 
 paul = paul.toString()
 
-console.log(paul);
-
-function trim(text) {
+function trim(text) { //fully functionaly (as far as I can tell. Returns array full of all unique words contained in fed string.)
+    var dictionary = [];
+    var j = 0;
+    var lead = 0;
+    var trail = 0;
+    var found = 0;
     for (var i = 0; i < text.length; ++i) {
         switch (text[i]) {
-            case 'a':
+            case ' ':
+                trail = lead;
+                lead = i + 1;//ok this looks dumb. I know it looks dumb. 
+                temp = text.substring(trail, lead - 1);//But this only works if I define lead as + 1 and then -1 the only time I use it. Dont judge Rat.js he is trying hard.
+                for (var u = 0; u < dictionary.length; ++u) {
+                    if (dictionary[u] == temp) {
+                        found = 1;
+                        break;
+                    }
+                }
+                if (found == 0) {
+                    dictionary[j++] = temp;
+                }
+                found = 0;
+                break;
+            case '.':
+            case '(':
+            case ')':
+            case ',':
+            case ':':
+            case ';':
+            case "'":
+            case '"':
+                console.log(text[i]);
+                text = text.substring(0, i).concat(text.substring(i + 1, text.length));
+                break;
+            default:
                 break;
         }
     }
+    return dictionary;
 }
+
+console.log(trim(paul));
 
 function libraryCreate(text) {
     for (var i = 0; i < text.length; i++) {
