8207ba79b4bcbdbcf9d3931ec9b0cfa013c4abf7
Flint <oliverflintrose@gmail.com>
2023-04-03 14:27:35 -0500

    Training data building function finished

--- rat.js
+++ rat.js
@@ -1,18 +1,20 @@
 var synaptic = require('synaptic');
 var fs = require('fs');
+const { train } = require('@tensorflow/tfjs');
+const { create } = require('domain');
 const learningRate = 0.3;
-const paul = fs.readFileSync('text/message.txt', error).toString().toLowerCase();
+const paul = fs.readFileSync('text/paulBorrego.txt', error).toString().toLowerCase();
 //Rat time. 
+var Neuron = synaptic.Neuron,
+    Layer = synaptic.Layer,
+    Network = synaptic.Network,
+    Trainer = synaptic.Trainer
 //RNN , Recurrent Neural Network 
 
 function error(err, inputD) {
     if (err) throw err;
     return inputD;
 }
-
-
-var testText = 'This. Is a decent test, of course I dont think its great? But like (butt) I think I need to kill the president. "Again?"';
-
 function punc(text) { //splits up input text to have spaces between , and . also removes all excessive punctuation and returns an array of each 'word'
     var tail = 0;
     var head = 0;
@@ -47,7 +49,6 @@ function punc(text) { //splits up input text to have spaces between , and . also
     }
     return returny;
 }
-
 function dictionaryAdd(text) { //takes an array full of transfered words and returns list of each word present in list
     var dictionary = [];
     var u = 0;
@@ -66,7 +67,6 @@ function dictionaryAdd(text) { //takes an array full of transfered words and ret
     }
     return dictionary;
 }
-
 function spaceDelete(text) { //takes an array and delets every space (just a brute force solution to a bug)
     var returnArray = [];
     var j = 0;
@@ -77,10 +77,6 @@ function spaceDelete(text) { //takes an array and delets every space (just a bru
     }
     return returnArray;
 }
-
-//console.log(spaceDelete(punc(paul)));
-var dictionary = dictionaryAdd(punc(paul));
-
 function roboTranslate(text, dictionary) { //takes an array full of words and returns an array full of dictionary number ==
     var roboText = []
     for (var i = 0; i < text.length; ++i) {
@@ -93,59 +89,45 @@ function roboTranslate(text, dictionary) { //takes an array full of words and re
     }
     return roboText;
 }
-
-console.log(roboTranslate(spaceDelete(punc(paul)), dictionary));
-
-/*
-function trim(text) { //fully functionaly (as far as I can tell. Returns array full of all unique words contained in fed string.)
-    var dictionary = [];
+function createData(text) { //takes input of robotranslated text array and returns 3d array containing [[5 words, next word],[...]]
+    var trainingData = [{}];
     var j = 0;
-    var lead = 0;
-    var trail = 0;
-    var found = 0;
     for (var i = 0; i < text.length; ++i) {
-        switch (text[i]) {
-            case ' ':
-                trail = lead;
-                lead = i + 1;//ok this looks dumb. I know it looks dumb. 
-                temp = text.substring(trail, lead - 1);//But this only works if I define lead as + 1 and then -1 the only time I use it. Dont judge Rat.js he is trying hard.
-                for (var u = 0; u < dictionary.length; ++u) {
-                    if (dictionary[u] == temp) {
-                        found = 1;
-                        break;
-                    }
-                }
-                if (found == 0) {
-                    dictionary[j++] = temp;
-                }
-                found = 0;
-                break;
-            case '.':
-            case '(':
-            case ')':
-            case ',':
-            case ':':
-            case ';':
-            case "'":
-            case '"':
-                text = text.substring(0, i).concat(text.substring(i + 1, text.length));
-                console.log(text);
-                break;
-            default:
-                break;
+        if (i > 5) {
+            trainingData[j++] =
+            {
+                input: [text[i - 5], text[i - 4], text[i - 3], text[i - 2], text[i - 1]],
+                output: [text[i]]
+            }
         }
     }
-    return dictionary;
+    return trainingData;
 }
-*/
-//const localDictionary = trim(paul);
-
-//console.log(localDictionary);
 
 
-var Neuron = synaptic.Neuron,
-    Layer = synaptic.Layer,
-    Network = synaptic.Network,
-    Trainer = synaptic.Trainer,
-    Architect = synaptic.Architect;
-var inputLayer = new Layer()
+var dictionary = dictionaryAdd(punc(paul));
+
+var beepBoopPaul = roboTranslate(spaceDelete(punc(paul)), dictionary);
+
+console.log(createData(beepBoopPaul));
+
+var inputLayer = new Layer(5);
+var invisibleLayer0 = new Layer(20);
+var invisibleLayer1 = new Layer(20);
+var invisibleLayer2 = new Layer(20);
+var outputLayer = new Layer(695);
+
+inputLayer.project(invisibleLayer0, Layer.connectionType.ALL_TO_ALL);
+invisibleLayer0.project(invisibleLayer1, Layer.connectionType.ALL_TO_ALL);
+invisibleLayer1.project(invisibleLayer2, Layer.connectionType.ALL_TO_ALL);
+invisibleLayer2.project(outputLayer, Layer.connectionType.ALL_TO_ALL);
+
+var paulOnlyRNN = new Network({
+    input: inputLayer,
+    hidden: [invisibleLayer0, invisibleLayer1, invisibleLayer2],
+    output: outputLayer
+})
+
+var paulTraining = new Trainer(paulOnlyRNN)
+
+var paulTrainingData = [];
