080f97653e3ae13a822d774a2aa54d07135fd564
Flint <oliverflintrose@gmail.com>
2023-04-05 14:19:20 -0500

    Hopefully the last time that I will have to clean up my language interperitation functions.

--- rat.js
+++ rat.js
@@ -1,6 +1,6 @@
 var synaptic = require('synaptic');
 var fs = require('fs');
-const { train } = require('@tensorflow/tfjs');
+var math = require('mathjs');
 const learningRate = 0.3;
 const talks = fs.readFileSync('text/talks.txt', error).toString().toLowerCase();
 //Rat time. 
@@ -14,46 +14,12 @@ function error(err, inputD) {
     if (err) throw err;
     return inputD;
 }
-function dictionaryTotal(array) {
-    var returnArray = [];
-    var present = 0;
-    for (var i = 0; i < array.length; ++i) {
-        for (var j = 0; j < returnArray.length; ++j) {
-            if (array[i] == returnArray[j]) {
-                present = 1;
-            }
-        }
-        if (present == 0) {
-            returnArray.push(array[i]);
-        }
-        present = 0;
-    }
-    return returnArray;
-}
 function textToArray(text) {
-    array = text.split(' '); //This, is, an, example., Alright,, lets, do, this.,
-    var temp;
-    /*
-    for (var i = 0; i < array.length; ++i) {
-        temp = array[i].at(-1);
-        if (temp == ',') {
-            array[i] = array[i].substring(0, array[i].length - 1);
-            array.splice(i + 1, 0, ',');
-            ++i;
-        }
-        else if (temp == '.') {
-            array[i] = array[i].substring(0, array[i].length - 1);
-            array.splice(i + 1, 0, '.');
-            ++i;
-        }
-    }
-    */
-    for (var i = 0; i < array.length; ++i) {
-        array[i] = array[i].replace(/["'“”\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s+/g, " ");
-    }
+    text = text.replace(/[!?"'“”\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s+/g, " ");
+    array = text.split(' ');
     return array;
 }
-function roboTranslate(text, dictionary) { //takes an array full of words and returns an array full of dictionary number ==
+function roboTranslate(array, dictionary) { //takes an array full of words and returns an array full of dictionary number ==
     var roboText = []
     for (var i = 0; i < array.length; ++i) {
         for (var j = 0; j < dictionary.length; ++j) {
@@ -64,44 +30,50 @@ function roboTranslate(text, dictionary) { //takes an array full of words and re
     }
     return roboText;
 }
-function humaTranslate(text, dictionary) {
+function humanTranslate(array, dictionary) { //takes an array full of locals in dictionary, returns string with ' ' between each element.
     var returnArray = [];
-    for (var i = 0; i < text.length; ++i) {
-        for (var j = 0; j < dictionary.length; ++j) {
-            if (text[i] == dictionary[j]) {
-                returnArray.push(j);
-            }
-        }
+    for (var i = 0; i < array.length; ++i) {
+        returnArray.push(dictionary[parseInt(array[i])]);
     }
-    return returnArray;
+    return returnArray.join(' ')
 }
-function createData(text, dictionary) { //takes input of robotranslated text array and returns 3d array containing [[5 words, next word],[...]]
+function createData(array, dictionary) { //takes input of robotranslated array and returns 3d array containing [[5 words, next word],[...]]
     var trainingData = [{}];
     var j = 0;
-    var temp = [];
+    var temp = Array(dictionary.length).fill(0);
     var output = [];
     for (var i = 0; i < dictionary.length; ++i) {
         temp[i] = 0;
     }
-    for (var i = 10; i < text.length; ++i) {
-        temp[parseInt(text[i - 1])] = 0;
-        temp[parseInt(text[i])] = 1;
+    for (var i = 10; i < array.length; ++i) {
+        temp[parseInt(array[i - 1])] = 0;
+        temp[parseInt(array[i])] = 1;
         output = [...temp];
         trainingData[j++] =
         {
-            input: [text[i - 10], text[i - 9], text[i - 8], text[i - 7], text[i - 6], text[i - 5], text[i - 4], text[i - 3], text[i - 2], text[i - 1]],
+            input: [array[i - 10], array[i - 9], array[i - 8], array[i - 7], array[i - 6], array[i - 5], array[i - 4], array[i - 3], array[i - 2], array[i - 1]],
             output: output
+
         }
     }
     return trainingData;
 }
-
-var dictionary = dictionaryTotal(textToArray(talks));
-
-var beepBoop = roboTranslate(textToArray(talks), dictionary);
+function outputCreate(array, size, NN) { // takes input of array of 10 robotranslated words, returns text of size
+    var outputArray = [...array];
+    var nextRun;
+    var nextWord;
+    for (var i = 0; i < size; ++i) {
+        nextRun = NN.activate(array);
+        nextWord = nextRun.indexOf(math.max(nextRun))
+        outputArray.push(nextWord);
+        array.push(nextWord);
+        array.shift();
+    }
+    return outputArray;
+}
 
 //console.log(humaTranslate(['i', 'am', 'an', 'optimist.', 'i', 'like', 'to', 'look', 'on', 'the'], dictionary))
-
+/*
 var input = 10;
 var blocks = 12;
 var output = dictionary.length;
@@ -142,14 +114,17 @@ var rat = new Network({
 })
 
 var ratTraining = new Trainer(rat, {
-    rat: .1,
-    iterations: 20000,
-    error: .005,
+    learningRate: .1,
+    iterations: 100000,
     shuffle: true,
     log: 1000,
+    schedule: {
+        every: 1, // repeat this task every 500 iterations
+        do: console.log('.')
+    }
 });
 
-
+/*
 var trainingData = createData(beepBoop, dictionary);
 
 ratTraining.train(trainingData);
@@ -157,37 +132,37 @@ ratTraining.train(trainingData);
 var exported = rat.toJSON();
 
 fs.writeFile('rat.json', JSON.stringify(exported), 'utf8', error);
+*/
 
-
-/*
 const ratImport = fs.readFileSync('rat.json', 'utf8', error)
 
 var ratImported = Network.fromJSON(JSON.parse(ratImport));
 
-var max = 0;
-var local = 0;
-console.log(dictionary.length);
-//var list = humaTranslate(['i', 'am', 'an', 'optimist.', 'i', 'like', 'to', 'look', 'on', 'the'], dictionary);
-var list = ([0, 1, 2, 3, 0, 4, 5, 6, 7, 8]);
-var output;
-var roboList = list;
-var length = 10;
+var arrayText = textToArray(talks);
 
-for (var i = 0; i < 200; ++i) {
-    var max = 0;
-    var local = 0;
-    output = ratImported.activate(list);
-    for (var j = 0; j < dictionary.length; ++j) {
-        if (output[j] > max) {
-            local = j;
-            max = output[j];
-        }
+var dictionary = [...new Set(arrayText)];
+
+var beepBoop = roboTranslate(arrayText, dictionary);
+
+var list = ([0, 1, 2, 3, 0, 4, 5, 6, 7, 8]);
+
+var outputRobo = outputCreate(list, 200, ratImported);
+
+var outputText = humanTranslate(outputRobo, dictionary);
+
+console.log(outputText);
+
+
+/*
+var test = ratImported.activate([3, 0, 4, 5, 6, 7, 8, 69, 0, 0]);
+var max = 0;
+var loc = 0;
+for (var i = 0; i < test.length; ++i) {
+    if (test[i] > max) {
+        loc = i;
+        max = test[i];
     }
-    for (var j = 0; j < length - 1; ++j) {
-        list[j] = list[j + 1];
-    }
-    list[9] = local;
-    roboList.push(local);
 }
-console.log(humaTranslate(roboList, dictionary));
+console.log(loc);
+
 */
