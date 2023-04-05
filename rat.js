fbfd5e5aeaa70b4c24d9e36dd3e25a2faa5d8402
Flint <oliverflintrose@gmail.com>
2023-04-05 07:33:57 -0500

    Ran for a good 14 hours, never ended training. Either an infinite loop was entered or I am going to have to figure out how I am going to train.

--- rat.js
+++ rat.js
@@ -14,15 +14,9 @@ function error(err, inputD) {
     if (err) throw err;
     return inputD;
 }
-function dictionaryTotal(text) {
-    var array = text.split(' ');
+function dictionaryTotal(array) {
     var returnArray = [];
     var present = 0;
-    for (var i = 2; i < array.length; ++i) {
-        array[i] = array[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s+/g, " ");
-    }
-    array.unshift('.');
-    array.unshift(',');
     for (var i = 0; i < array.length; ++i) {
         for (var j = 0; j < returnArray.length; ++j) {
             if (array[i] == returnArray[j]) {
@@ -39,6 +33,7 @@ function dictionaryTotal(text) {
 function textToArray(text) {
     array = text.split(' '); //This, is, an, example., Alright,, lets, do, this.,
     var temp;
+    /*
     for (var i = 0; i < array.length; ++i) {
         temp = array[i].at(-1);
         if (temp == ',') {
@@ -52,6 +47,7 @@ function textToArray(text) {
             ++i;
         }
     }
+    */
     for (var i = 0; i < array.length; ++i) {
         array[i] = array[i].replace(/["'“”\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s+/g, " ");
     }
@@ -69,11 +65,15 @@ function roboTranslate(text, dictionary) { //takes an array full of words and re
     return roboText;
 }
 function humaTranslate(text, dictionary) {
-    var returnString = '';
+    var returnArray = [];
     for (var i = 0; i < text.length; ++i) {
-        returnString = returnString + ' ' + dictionary[text[i]];
+        for (var j = 0; j < dictionary.length; ++j) {
+            if (text[i] == dictionary[j]) {
+                returnArray.push(j);
+            }
+        }
     }
-    return returnString;
+    return returnArray;
 }
 function createData(text, dictionary) { //takes input of robotranslated text array and returns 3d array containing [[5 words, next word],[...]]
     var trainingData = [{}];
@@ -83,26 +83,27 @@ function createData(text, dictionary) { //takes input of robotranslated text arr
     for (var i = 0; i < dictionary.length; ++i) {
         temp[i] = 0;
     }
-    for (var i = 5; i < text.length; ++i) {
+    for (var i = 10; i < text.length; ++i) {
         temp[parseInt(text[i - 1])] = 0;
         temp[parseInt(text[i])] = 1;
         output = [...temp];
         trainingData[j++] =
         {
-            input: [text[i - 5], text[i - 4], text[i - 3], text[i - 2], text[i - 1]],
+            input: [text[i - 10], text[i - 9], text[i - 8], text[i - 7], text[i - 6], text[i - 5], text[i - 4], text[i - 3], text[i - 2], text[i - 1]],
             output: output
         }
     }
     return trainingData;
 }
 
-var dictionary = dictionaryTotal(talks);
+var dictionary = dictionaryTotal(textToArray(talks));
 
 var beepBoop = roboTranslate(textToArray(talks), dictionary);
 
-//var beepBoop = roboTranslate(talks, dictionary);
-var input = 5;
-var blocks = 7;
+//console.log(humaTranslate(['i', 'am', 'an', 'optimist.', 'i', 'like', 'to', 'look', 'on', 'the'], dictionary))
+
+var input = 10;
+var blocks = 12;
 var output = dictionary.length;
 //layer init
 var inputLayer = new Layer(input); //Input, for now will be first 5 words that extend each step
@@ -140,7 +141,14 @@ var rat = new Network({
     output: outputLayer
 })
 
-var ratTraining = new Trainer(rat);
+var ratTraining = new Trainer(rat, {
+    rat: .1,
+    iterations: 20000,
+    error: .005,
+    shuffle: true,
+    log: 1000,
+});
+
 
 var trainingData = createData(beepBoop, dictionary);
 
@@ -149,21 +157,23 @@ ratTraining.train(trainingData);
 var exported = rat.toJSON();
 
 fs.writeFile('rat.json', JSON.stringify(exported), 'utf8', error);
+
+
 /*
 const ratImport = fs.readFileSync('rat.json', 'utf8', error)
 
 var ratImported = Network.fromJSON(JSON.parse(ratImport));
 
-var log = ratImported.activate([0, 45, 7, 46, 5]); //47
-
 var max = 0;
 var local = 0;
 console.log(dictionary.length);
-var list = [1100, 1430, 13, 32, 5];
+//var list = humaTranslate(['i', 'am', 'an', 'optimist.', 'i', 'like', 'to', 'look', 'on', 'the'], dictionary);
+var list = ([0, 1, 2, 3, 0, 4, 5, 6, 7, 8]);
 var output;
-var roboList = [1100, 1430, 13, 32, 5];
+var roboList = list;
+var length = 10;
 
-for (var i = 0; i < 20; ++i) {
+for (var i = 0; i < 200; ++i) {
     var max = 0;
     var local = 0;
     output = ratImported.activate(list);
@@ -173,11 +183,10 @@ for (var i = 0; i < 20; ++i) {
             max = output[j];
         }
     }
-    list[0] = list[1];
-    list[1] = list[2];
-    list[2] = list[3];
-    list[3] = list[4];
-    list[4] = local;
+    for (var j = 0; j < length - 1; ++j) {
+        list[j] = list[j + 1];
+    }
+    list[9] = local;
     roboList.push(local);
 }
 console.log(humaTranslate(roboList, dictionary));
